05e0af4d9d36b4b820775e8e05eb053c32144b2d
TiagoBarbosa88 <sombr43@gmail.com>
2023-02-10 07:32:39 -0300

    sistema bugado

--- petshop/src/app/pages/store/products-page/products-page.component.ts
+++ petshop/src/app/pages/store/products-page/products-page.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
-
+import { Product } from 'src/app/models/product.model';
 
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
-
+  styleUrls: ['./products-page.component.css']
 })
 export class ProductsPageComponent implements OnInit {
-  public products$: Observable<any[]> | undefined;
+  products$: Observable<Product[]> = null;
 
-  constructor(private data: DataService) { }
+  constructor(private service: DataService) { }
 
-  ngOnInit(): void {
-    this.products$ = this.data.getProducts();
+  ngOnInit() {
+    this.products$ = this.service.getProducts();
   }
 
 }
